7e525296b7c20d77f077fa2bc9801f9bf0c52c2c
matheus engel <matheusengelfl@gmail.com>
2025-11-25 17:09:03 -0300

    feat: começa com as falhas

--- src/main/java/org/example/repository/FalhaRepositoryImpl.java
+++ src/main/java/org/example/repository/FalhaRepositoryImpl.java
@@ -1,4 +1,54 @@
 package org.example.repository;
 
+import org.example.database.Conexao;
+import org.example.model.Falha;
+
+import java.math.BigDecimal;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FalhaRepositoryImpl {
+
+    public Falha registrarFalha(Falha falha)throws SQLException{
+        String query = """
+                INSERT INTO Falha (equipamentoId, dataHoraOcorrencia, descricao, criticidade, status, tempoParadaHoras)
+                VALUES 
+                (?,?,?,?,'ABERTA',?)
+                """;
+    try (Connection conn = Conexao.conectar();
+         PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)){
+        stmt.setLong(1, falha.getEquipamentoId());
+        stmt.setObject(2, falha.getDataHoraOcorrencia());
+        stmt.setString(3, falha.getDescricao());
+        stmt.setString(4, falha.getCriticidade());
+        stmt.setBigDecimal(5, falha.getTempoParadaHoras());
+        stmt.executeUpdate();
+
+        ResultSet rs = stmt.getGeneratedKeys();
+
+        if (rs.next()){
+            falha.setId(rs.getLong(1));
+        }
+
+        falha.setStatus("ABERTA");
+
+    }catch (SQLException e){
+        e.printStackTrace();
+    }
+   return falha;
+    }
+
+    public <FalhaCritica> List<FalhaCritica> falhaCriticaList() throws SQLException{
+        String query = """
+                SELECT equipamentoId, dataHoraOcorrencia, descricao, criticidade, status, tempoParadaHoras
+                FROM Falha 
+                WHERE criticidade = 'CRITICA'        
+                """;
+        List<FalhaCritica> falhaCriticas = new ArrayList<>();
+
+
+    return falhaCriticas;
+    }
 }
--- src/main/java/org/example/service/falha/FalhaServiceImpl.java
+++ src/main/java/org/example/service/falha/FalhaServiceImpl.java
@@ -1,13 +1,22 @@
 package org.example.service.falha;
 
 import org.example.model.Falha;
+import org.example.repository.FalhaRepositoryImpl;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class FalhaServiceImpl implements FalhaService{
+    FalhaRepositoryImpl falhaRepository = new FalhaRepositoryImpl();
+
     @Override
     public Falha registrarNovaFalha(Falha falha) throws SQLException {
+        try {
+            Falha falhaSalva = falhaRepository.registrarFalha(falha);
+            return falhaSalva;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 
